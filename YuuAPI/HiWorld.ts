466571c3b17ff43df1b9413929081a71b3dce2f8
GalacticVictoria <109833190+GalacticVictoria@users.noreply.github.com>
2026-06-18 14:13:30 -0400

    Add files via upload

---
+++ YuuAPI/HiWorld.ts
@@ -0,0 +1,10 @@
+import { inWorldConsole } from "./Yuu API/Console";
+import { registerStart } from "./Yuu API/RegisterStart";
+
+
+registerStart
+function start () {
+inWorldConsole.visible(true);
+
+console.log('You Did A Thing!');
+}
