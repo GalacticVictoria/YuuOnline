b0e9bf49f87df26d385995851c3832506699f965
GalacticVictoria <109833190+GalacticVictoria@users.noreply.github.com>
2026-06-26 10:33:24 -0400

    Added plane

--- HiWorld.ts
+++ HiWorld.ts
@@ -1,6 +1,7 @@
 import { Vector3 } from "./Yuu API/Basic Types/Vector3";
 import { inWorldConsole } from "./Yuu API/Console";
 import { registerStart } from "./Yuu API/RegisterStart";
+import { spawnPrimitive } from "./Yuu API/SpawnPrimitive";
 
 
 registerStart(start);
@@ -8,4 +9,6 @@ function start() {
     inWorldConsole.visible(true, new Vector3(0,1.5,-1.5));
 
     console.log('You Did A Thing!');
+
+    spawnPrimitive.plane;
 }
